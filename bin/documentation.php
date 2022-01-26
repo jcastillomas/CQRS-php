0bf1e3e1af2dc7f577829ff149d9eacf360dc3cd
jcastillo <jcastillomas@gmail.com>
2022-01-26 12:01:51 +0100

    feature(swagger): autoload documentation

--- bin/documentation.php
+++ bin/documentation.php
@@ -3,4 +3,8 @@
 require("vendor/autoload.php");
 
 $openapi = \OpenApi\Generator::scan(['./src/UI/Http/Rest']);
-echo $openapi->toJson();
+$response = $openapi->toJson();
+
+$fp = fopen('./public/swagger-ui/swagger.json', 'w');
+fwrite($fp, $response);
+fclose($fp);
